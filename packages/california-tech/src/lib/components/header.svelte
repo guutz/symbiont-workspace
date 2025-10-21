711c2989f6484b27f9c4db79e7b739f965fabece
Michael Gutierrez <findmagpie@gmail.com>
2025-10-21 15:23:19 -0700

    begin refactor of tech nav header

--- packages/california-tech/src/lib/components/header.svelte
+++ packages/california-tech/src/lib/components/header.svelte
@@ -2,12 +2,21 @@
   import Masthead from '$lib/components/masthead.svelte';
   import ScrollButtons from '$lib/components/scroll_buttons.svelte';
   import DefaultNav from '$lib/components/header/DefaultNav.svelte';
+  import { NavigationMenu } from "bits-ui";
+  
+  
 </script>
 
 <header id="header" class="w-screen z-40" aria-label="Header Navigation">
   <Masthead />
   <div class="relative py-2 min-h-4rem max-h-16">
-    <DefaultNav />
+    <NavigationMenu.Root>
+      <NavigationMenu.List class="max-w-7xl mx-auto px-4 sm:px-8 h-full">
+        <NavigationMenu.Item class="flex-grow">
+          hi
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
   </div>
 </header>
 
